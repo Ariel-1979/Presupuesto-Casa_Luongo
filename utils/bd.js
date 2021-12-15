8c4fd50e32d1085880df8e89e5ea068b36c2caf8
Ariel-1979 <arieldominguez001@gmail.com>
2021-12-14 21:18:35 -0300

    ".ENV"

--- utils/bd.js
+++ utils/bd.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql');
 const util = require('util');
 
-const pool = mysql.createPool ({
-    host: process.env.BD_HOST || 'localhost',
-    port: process.env.BD_PORT || 3306,
-    password : process.env.BD_PASSWORD || '',
-    user : process.env.BD_USER || 'root',
-    database : process.env.BD_NAME || 'presupuestos',
-    connectionLimit : 10,
+const pool = mysql.createPool({
+  host: process.env.BD_HOST,
+  port: process.env.BD_PORT,
+  password: process.env.BD_PASSWORD,
+  user: process.env.BD_USER,
+  database: process.env.BD_NAME,
+  connectionLimit: 10,
 });
 
 pool.query = util.promisify(pool.query);
